f18733009d0ee63b51586aa706953a89fb77ddc1
Adwitya02 <149108559+Adwitya02@users.noreply.github.com>
2025-11-27 22:59:19 +0530

    Remove Firebase configuration and imports
    Removed Firebase import URLs and configuration values.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from
+import { getAuth } from 
+import { getFirestore } from 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDF2ltsfcjgRu4qn83Sic_kVKnJWWnH3GE",
-  authDomain: "blackjackwebapp-adwitya.firebaseapp.com",
-  projectId: "blackjackwebapp-adwitya",
-  storageBucket: "blackjackwebapp-adwitya.appspot.com",
-  messagingSenderId: "628143120072",
-  appId: "1:628143120072:web:7ee3925e15c59f4a06cffd"
+  apiKey:
+  authDomain:
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
 };
 
 export const app = initializeApp(firebaseConfig);
